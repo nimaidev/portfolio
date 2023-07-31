588d7ca702d25332368a60a42a335efab4e434ad
0x4E43 <nimai.c@iqnext.io>
2023-07-31 14:11:32 +0530

    Blog list not fetching issue fixed, API Updated"

--- pages/api/get-blogs.ts
+++ pages/api/get-blogs.ts
@@ -4,7 +4,7 @@ import fetch from 'isomorphic-unfetch';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const username = req.query.username as string;
   console.log(username)
-  const response = await fetch(`https://dev.to/api/articles/latest?username=${username}`);
+  const response = await fetch(`https://dev.to/api/articles?username=${username}`);
   const data = await response.json();
   res.status(200).json(data);
 }
