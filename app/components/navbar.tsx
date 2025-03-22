2178cb4793cd8e95a6a10e31c459e8f8ee8ab34f
NimaiDev <nimaic.dev@gmail.com>
2025-03-22 22:32:40 +0530

    Nav Bar Ui fix

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,16 @@
 import DarkModeBtn from "../darkModeBtn";
 
 const NavBar = () => {
-  return (
-    <div className="navbar shadow-sm">
-      <div className="flex-1">
-        <h1 className="text-white mb-4">Nimai Charan</h1>
-      </div>
-      <div className="flex-none">
-        <DarkModeBtn />
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar shadow-sm border-b border-app-teal flex items-center">
+            <div className="flex-1">
+                <h1 className="text-white">Nimai Charan</h1>
+            </div>
+            <div className="flex-none">
+                <DarkModeBtn />
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
