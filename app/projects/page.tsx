6e6a6656b4e7e728b40ab7ebc4b4dabf334af829
NimaiDev <nimaic.dev@gmail.com>
2025-04-19 00:14:08 +0530

    minor UI fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,8 +8,11 @@ export default function Projects() {
       <div className="sticky top-0 z-50">
         <NavBar />
       </div>
-      <div className="w-full max-w-6xl mx-auto px-4 md:pt-[100px] sm:pt-[450px] min-h-screen">
-        <p className="text-white">text</p>
+      <div className="w-full max-w-6xl mx-auto px-4 md:pt-[100px] sm:pt-[450px] min-h-screen font-firacode">
+        <div>
+          <h1 className="text-3xl text-app-primary dark:text-app-teal">Personal projects.</h1>
+          <p>List of my weekend time-pass {"<"}3 </p>
+        </div>
         <Footer />
       </div>
       <div className="flex dark:bg-app-dark min-h-screen">
